d62983d56784bf8d1836bc9e05d3e94d93b1b4c9
Christian Burger <christian@krikkel.de>
2015-06-24 14:22:22 +0200

    class Num: added tests for addition and multiplication

--- tests/test.hs
+++ tests/test.hs
@@ -1,12 +1,14 @@
 import Test.Tasty
 import Test.Tasty.QuickCheck as QuickCheck
-
+-- import Test.Tasty.HUnit as HUnit
 import System.Clock
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Try" [qcAbsSignum]
+tests = testGroup "All tests" [quickCheckTests]
+
+quickCheckTests = testGroup "QuickCheck tests" [qcNumClass]
 
 instance Arbitrary TimeSpec where
   arbitrary = do
@@ -14,8 +16,20 @@ instance Arbitrary TimeSpec where
     nan <- arbitrarySizedIntegral
     return $ TimeSpec sec nan
 
-qcAbsSignum = testGroup "quick checking"
+qcNumClass = testGroup "quick checking"
   [ 
-    QuickCheck.testProperty "x = abs(x) * signum (x)" $ 
-      \x -> (x :: TimeSpec) == (abs x) * (signum x)
+    QuickCheck.testProperty "x = abs(x) * signum(x)" $
+      \ x -> (x :: TimeSpec) == (abs x) * (signum x)
+  , QuickCheck.testProperty "integer addition equals TimeSpec addition" $
+      \ x y -> x + y == timeSpecAsNanoSecs (fromInteger x + fromInteger y)
+  , QuickCheck.testProperty "rational multiplication equals TimeSpec multiplication" $
+      \ x y ->
+        let
+          rationalMul = truncate ((x :: Rational) * (y :: Rational) * (10^9))
+	  timespecMul = timeSpecAsNanoSecs (
+              fromInteger (truncate (x * 10^9))
+            * fromInteger (truncate (y * 10^9)))
+	in
+          -- there is some rounding error, we cannot compare directly with ==
+	  abs (rationalMul - timespecMul) <= 10
   ]
