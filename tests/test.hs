6a10908b59206f39f3cbc16ab4fcbc4bc41626e3
Christian Burger <christian@krikkel.de>
2015-06-25 10:50:25 +0200

    added three more tests
    1) testing `negate`
    2) testing `Eq` and `Ord` instance
    3) testing subtraction

--- tests/test.hs
+++ tests/test.hs
@@ -1,6 +1,7 @@
 import Test.Tasty
 import Test.Tasty.QuickCheck as QuickCheck
 import Data.Fixed
+import Data.List
 -- import Test.Tasty.HUnit as HUnit
 import System.Clock
 
@@ -10,14 +11,19 @@ instance Arbitrary TimeSpec where
     nan <- arbitrarySizedIntegral
     return $ TimeSpec sec nan
 
-main = defaultMain tests
+main = defaultMain adjustOption (QuickCheckTests 100000 +) $ tests
 
 tests :: TestTree
-tests = testGroup "All tests" [numClassTests]
+tests = testGroup "All tests" [numClassTests, eqOrdClassesTests]
 
 numClassTests = testGroup "Num class tests" [
   -- let's make at least 100,000 tests
-  adjustOption (QuickCheckTests 100000 +) $ qcNumClass
+  qcNumClass
+  ]
+
+eqOrdClassesTests = testGroup "Eq and Ord instance tests" [
+  -- let's make at least 100,000 tests
+  qcEqOrdClass
   ]
 
 qcNumClass = testGroup "QuickCheck"
@@ -26,6 +32,8 @@ qcNumClass = testGroup "QuickCheck"
       \ x -> (x :: TimeSpec) == (abs x) * (signum x)
   , QuickCheck.testProperty "integer addition equals TimeSpec addition" $
       \ x y -> x + y == timeSpecAsNanoSecs (fromInteger x + fromInteger y)
+  , QuickCheck.testProperty "integer substraction equals TimeSpec addition" $
+      \ x y -> x - y == timeSpecAsNanoSecs (fromInteger x - fromInteger y)
   , QuickCheck.testProperty
       "rational multiplication equals TimeSpec multiplication" $
       \ x y ->
@@ -36,4 +44,13 @@ qcNumClass = testGroup "QuickCheck"
             * fromInteger (truncate (y * 10^9)))
         in
           rationalMul == timespecMul
+  , QuickCheck.testProperty "neg(neg(x)) = x" $
+      \ x -> negate (negate x :: TimeSpec) == x
+  ]
+
+qcEqOrdClass = testGroup "QuickCheck"
+  [
+    QuickCheck.testProperty
+      "random list of TimeSpecs is sorted like equivalent list of integers" $
+      \ x -> sort (x :: [TimeSpec]) == map (fromInteger) (sort (map timeSpecAsNanoSecs x))
   ]
