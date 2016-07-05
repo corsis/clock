174f3ab0718169441c396f15d9f9906e8d7d103d
Cetin Sert <cetin.sert@gmail.com>
2016-07-05 02:14:18 +0200

    re-align test.hs layout; replace obsolete calls

--- tests/test.hs
+++ tests/test.hs
@@ -14,46 +14,25 @@ instance Arbitrary TimeSpec where
 main = defaultMain (adjustOption (QuickCheckTests 100000 +) $ tests)
 
 tests :: TestTree
-tests = testGroup "All tests" [numInstanceTests, eqOrdInstancesTests]
+tests  = testGroup "All tests" [numInstanceTests, ordInstanceTests]
 
-numInstanceTests = testGroup "Num class tests" [
-  -- let's make at least 100,000 tests
-  qcNumInstance
-  ]
-
-eqOrdInstancesTests = testGroup "Eq and Ord instance tests" [
-  -- let's make at least 100,000 tests
-  qcEqOrdInstance
-  ]
+numInstanceTests = testGroup "Num instance tests" [qcNumInstance]
+ordInstanceTests = testGroup "Ord instance tests" [qcOrdInstance]
 
 qcNumInstance = testGroup "QuickCheck"
   [ 
-    QuickCheck.testProperty "x = abs(x) * signum(x)" $
-      \ x -> (x :: TimeSpec) == (abs x) * (signum x)
-  , QuickCheck.testProperty "integer addition equals TimeSpec addition" $
-      \ x y -> x + y == timeSpecAsNanoSecs (fromInteger x + fromInteger y)
-  , QuickCheck.testProperty "integer substraction equals TimeSpec addition" $
-      \ x y -> x - y == timeSpecAsNanoSecs (fromInteger x - fromInteger y)
-  , QuickCheck.testProperty
-      "rational multiplication equals TimeSpec multiplication" $
+    QuickCheck.testProperty "x = abs(x) * signum(x)"                                 $ \ x   -> (x :: TimeSpec) == (abs x) * (signum x)
+  , QuickCheck.testProperty "integer     addition equals TimeSpec     addition"      $ \ x y -> x + y == toNanoSecs (fromInteger x + fromInteger y)
+  , QuickCheck.testProperty "integer substraction equals TimeSpec subtracttion"      $ \ x y -> x - y == toNanoSecs (fromInteger x - fromInteger y)
+  , QuickCheck.testProperty "rational multiplication equals TimeSpec multiplication" $
       \ x y ->
-        let
-          rationalMul = truncate ((x :: Nano) * (y :: Nano) * (10^9))
-          timespecMul = timeSpecAsNanoSecs (
-              fromInteger (truncate (x * 10^9))
-            * fromInteger (truncate (y * 10^9)))
-        in
-          rationalMul == timespecMul
-  , QuickCheck.testProperty "neg(neg(x)) = x" $
-      \ x -> negate (negate x :: TimeSpec) == x
+        let rationalMul = truncate                         ((x :: Nano) *                       (y :: Nano) * (10^9))
+            timespecMul = toNanoSecs (fromInteger (truncate (x * 10^9)) * fromInteger (truncate (y * 10^9)))
+        in  rationalMul == timespecMul
+  , QuickCheck.testProperty "neg(neg(x)) = x" $ \ x -> negate (negate x :: TimeSpec) == x
   ]
 
-qcEqOrdInstance = testGroup "QuickCheck"
+qcOrdInstance = testGroup "QuickCheck"
   [
-    QuickCheck.testProperty
-      "random list of TimeSpecs is sorted like equivalent list of integers" $
-      \ x ->
-        sort (x :: [TimeSpec])
-        ==
-        map (fromInteger) (sort (map timeSpecAsNanoSecs x))
+    QuickCheck.testProperty "random list of TimeSpecs is sorted like equivalent list of integers" $ \ x -> sort (x :: [TimeSpec]) == map (fromInteger) (sort (map toNanoSecs x))
   ]
